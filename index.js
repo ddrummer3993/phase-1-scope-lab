4ed83612dbfceaee5e65a7233e7f6e98872bed8e
David Drummer <david.b.drummer@gmail.com>
2022-02-02 15:49:30 -0500

    commit tree ea74686a967338f746cccf99e68a87764072e590

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+// declare variable customerName
+
+var customerName = 'bob';
+
+// create upperCaseCustomerName function
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+};
+
+//create function setBestCustomer
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+};
+
+//create overwriteBestCustomer function
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+};
+
+// create leastFavoriteCustomer and changeLeastFavoriteCustomer function
+
+const leastFavoriteCustomer = 'Bob';
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'Jim';
+};
